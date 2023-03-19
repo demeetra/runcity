8bb699f758c209a5fcd42f8c673a1688bbe404d0
demeetra <nad.vas.2000@yandex.ru>
2023-03-19 15:37:35 +0300

    use react=18 api for web

--- index.web.js
+++ index.web.js
@@ -1,10 +1,15 @@
+import React from 'react';
 import {AppRegistry} from 'react-native';
+import {createRoot} from 'react-dom/client';
 import App from './App';
 if (module.hot) {
   module.hot.accept();
 }
 AppRegistry.registerComponent('React Native Web', () => App);
+createRoot(document.getElementById('app-root')).render(<App />);
+/*
 AppRegistry.runApplication('React Native Web', {
   initialProps: {},
   rootTag: document.getElementById('app-root'),
 });
+*/

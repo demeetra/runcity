845fb8612936e981dee7a434f54d57ac5b8205d9
demeetra <nad.vas.2000@yandex.ru>
2023-06-19 00:01:12 +0300

    better web width

--- src/components/T_Events.tsx
+++ src/components/T_Events.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {FlatList, StyleSheet, View} from 'react-native';
+import {FlatList, Platform, StyleSheet, View} from 'react-native';
 import {O_Card} from '../components/O_Card';
 import {O_NoData} from '../components/O_NoData.tsx';
 
@@ -8,16 +8,35 @@ export const T_Events = ({events}) => {
     return <O_NoData />;
   }
   return (
-    <FlatList
-      data={events}
-      keyExtractor={({id}) => id}
-      renderItem={props => <O_Card {...props} />}
-      ItemSeparatorComponent={() => <View style={styles.separator} />}
-    />
+    <View style={styles.containter}>
+      <FlatList
+        data={events}
+        contentContainerStyle={styles.content}
+        keyExtractor={({id}) => id}
+        renderItem={props => <O_Card {...props} />}
+        ItemSeparatorComponent={() => <View style={styles.separator} />}
+      />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  containter: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    ...Platform.select({
+      web: {
+        minWidth: 300,
+      },
+    }),
+  },
+  content: {
+    ...Platform.select({
+      web: {
+        maxWidth: 364,
+      },
+    }),
+  },
   separator: {
     height: 32,
   },
